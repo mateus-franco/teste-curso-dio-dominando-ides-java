557464ee03c69fc5d5f104a5366e9d99098cda9b
mateus-franco <mateus.afranco@gmail.com>
2021-07-19 21:29:57 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,28 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		System.out.println(gato);
+		
+		
+		
+		
+		
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello World! " + ( a+b));*/
+		// TODO Auto-generated method stub
+
+	}
+	
+	class Livros {
+		private String nome;
+		private String npag;
+	}
+
+}
